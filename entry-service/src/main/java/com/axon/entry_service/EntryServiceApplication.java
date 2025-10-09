19789076d9472a0cc245a7733e07c29df9184718
yangnail <wardingplz@naver.com>
2025-10-09 15:09:34 +0900

    feat: Initial mono repo structure

---
+++ entry-service/src/main/java/com/axon/entry_service/EntryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.axon.entry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryServiceApplication.class, args);
+	}
+
+}
