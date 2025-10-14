ff235379d3ea39031665370605c6f92d7f522a42
yangnail <wardingplz@naver.com>
2025-10-14 09:25:52 +0900

    refactor(entry-service): refactor controller name, added static html file for event page
    '

--- entry-service/src/main/java/com/axon/entry_service/config/Security_Config.java
+++ entry-service/src/main/java/com/axon/entry_service/config/Security_Config.java
@@ -1,23 +1,34 @@
 package com.axon.entry_service.config;
 
+import com.axon.entry_service.config.auth.JwtAuthenticationFilter;
+import com.axon.entry_service.config.auth.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class Security_Config {
-    //postman
+
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/kafka/**")
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/v1/entries").authenticated()
+                        .anyRequest().permitAll() // 다른 모든 요청은 일단 허용
                 )
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/kafka/**").permitAll()
-                        .anyRequest().authenticated()
-                );
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ entry-service/src/main/java/com/axon/entry_service/dto/EntryRequestDto.java
@@ -0,0 +1,9 @@
+package com.axon.entry_service.dto;
+
+import lombok.Data;
+
+@Data
+public class EntryRequestDto {
+    private int eventId;
+    private int productId;
+}
