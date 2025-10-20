35bb67a636c2bf495918c06a4a02567d95f896cf
yangnail <wardingplz@naver.com>
2025-10-20 11:01:38 +0900

    feat(entity): camapaigns, events 엔티티 개발

--- core-service/src/main/java/com/axon/core_service/domain/Event.java
+++ core-service/src/main/java/com/axon/core_service/domain/Event.java
@@ -1,7 +1,7 @@
 package com.axon.core_service.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.axon.core_service.domain.campaign.CampaignStatus;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,13 +11,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
+@Table(name = "events")
 public class Event {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String eventName;
+
+    private int campaignId; // 캠페인 아이디
+    private String eventName; // 이벤트 이름
 
     private int limitCount; // 선착순 제한 인원
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private CampaignStatus campaignStatus;
 
 }
